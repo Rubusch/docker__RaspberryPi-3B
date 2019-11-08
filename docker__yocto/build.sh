cef732ba1dda3c40f5c206769d284538bd1b79b9
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-08 11:26:53 +0100

    build.sh: build preparation started

--- docker__yocto/build.sh
+++ docker__yocto/build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -e
+## resources:
+## https://jumpnowtek.com/rpi/Raspberry-Pi-Systems-with-Yocto.html
 cd poky
-git checkout -b warrior origin/warrior
-source oe-init-build-env
+
+export BUILDDIR=/mnt/build
+source oe-init-build-env $BUILDDIR
+
+cp -af ~/poky/meta-rpi/conf/local.conf.sample $BUILDDIR/conf/local.conf
+cp -af ~/poky/meta-rpi/conf/bblayers.conf.sample $BUILDDIR/conf/bblayers.conf
+
+## WARNING: Do not include meta-yocto-bsp in your bblayers.conf.
+## The Yocto BSP requirements for the Raspberry Pi are in meta-raspberrypi
+# TODO adjust bblayers
+
 
 ## TODO yocto layer, params, etc, setup for rpi3b, repo...
 bitbake core-image minimal
