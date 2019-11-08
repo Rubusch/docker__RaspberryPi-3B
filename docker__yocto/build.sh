ed036f198ff9919c0362a4dd9738f4f77ef58bd9
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-08 12:03:03 +0100

    build.sh: update with specific configs

--- docker__yocto/build.sh
+++ docker__yocto/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 ## resources:
 ## https://jumpnowtek.com/rpi/Raspberry-Pi-Systems-with-Yocto.html
-cd poky
+export BUILDDIR=~/poky/build
+chown $(whoami):$(whoami) -R $BUILDDIR
 
-export BUILDDIR=/mnt/build
+cd poky
 source oe-init-build-env $BUILDDIR
 
 cp -af ~/poky/meta-rpi/conf/local.conf.sample $BUILDDIR/conf/local.conf
@@ -11,11 +12,25 @@ cp -af ~/poky/meta-rpi/conf/bblayers.conf.sample $BUILDDIR/conf/bblayers.conf
 
 ## WARNING: Do not include meta-yocto-bsp in your bblayers.conf.
 ## The Yocto BSP requirements for the Raspberry Pi are in meta-raspberrypi
-# TODO adjust bblayers
+
+## adjust bblayers.conf
+sed -i 's/poky-warrior/poky/g' $BUILDDIR/conf/bblayers.conf
+sed -i 's/\/rpi\//\/poky\//g' $BUILDDIR/conf/bblayers.conf
+
+## adjust local.conf
+## raspberrypi (BCM2835)
+## raspberrypi0 (BCM2835)
+## raspberrypi0-wifi (BCM2835)
+## raspberrypi2 (BCM2836 or BCM2837 v1.2+)
+## raspberrypi3 (BCM2837)
+## raspberrypi-cm (BCM2835)
+## raspberrypi-cm3 (BCM2837)
+sed -i '/^MACHINE = "/s/.*/MACHINE = "raspberrypi3"/g' $BUILDDIR/conf/local.conf
+
 
 ## source again, before start building
 source oe-init-build-env $BUILDDIR
 
 # TODO build
 
-bitbake core-image minimal
+bitbake console-image
