939e0b8edad572a01702b95c5688cd017e900df2
Lothar Rubusch <l.rubusch@gmx.ch>
2020-05-10 20:42:47 +0200

    build.sh: update

--- docker__buildroot/build.sh
+++ docker__buildroot/build.sh
@@ -4,6 +4,7 @@ export MY_HOME=/home/$(whoami)
 
 sudo chown $(whoami):$(whoami) -R ${MY_HOME}/buildroot/dl
 sudo chown $(whoami):$(whoami) -R ${MY_HOME}/buildroot/output
+
 cd ${MY_HOME}/buildroot
 make ${DEFCONFIG}
 make -j8
