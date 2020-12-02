53f88b3f7b44b77f2c7981a5a6c6b4a800773928
Lothar Rubusch <l.rubusch@gmx.ch>
2020-12-02 19:21:17 +0100

    buildroot: build.sh - fix default defconfig

--- docker__buildroot/build.sh
+++ docker__buildroot/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
-export DEFCONFIG=lothars__raspberrypi3_defconfig
+export DEFCONFIG=raspberrypi3_defconfig
 export MY_HOME=/home/$(whoami)
 
 sudo chown $(whoami):$(whoami) -R ${MY_HOME}/buildroot/dl
 sudo chown $(whoami):$(whoami) -R ${MY_HOME}/buildroot/output
 
 cd ${MY_HOME}/buildroot
-make ${DEFCONFIG}
+make defconfig ${DEFCONFIG}
 make -j8
